ec1445589710649a18a1634a723d24cba93ac341
dimaswicaksono138-cloud <dimaswicaksono138@gmail.com>
2026-07-21 16:18:01 +0700

    Fix TypeScript error for Vercel build

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ function App() {
     const opt = {
       margin:       0.5,
       filename:     `${title}.pdf`,
-      image:        { type: 'jpeg', quality: 0.98 },
+      image:        { type: 'jpeg' as const, quality: 0.98 },
       html2canvas:  { scale: 2 },
       jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
     };
